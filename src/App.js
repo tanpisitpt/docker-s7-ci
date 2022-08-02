6af796ab2feb9752111768dfbf04befd4a8e7b28
tanpisitpt <tanpisit.pt@gmail.com>
2022-08-02 14:53:28 +0700

    feat: change

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Changed!
+          Changed! again
         </p>
         <a
           className="App-link"
